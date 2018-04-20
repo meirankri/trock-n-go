c9f90785d3442dcbc67825e70700891ff53949be
meirankri <35308733+meirankri@users.noreply.github.com>
2018-04-20 14:41:34 +0200

    Add files via upload

---
+++ form_offers.php
@@ -0,0 +1,62 @@
+<?php require_once 'connect_truc-go.php';
+
+ ?>
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="bootstrap.css">
+     <script src="bootstrap.js">
+
+     </script>
+     <title>Document</title>
+   </head>
+   <body>
+     <form  action="confirm_offer.php" method="post" class="m-5">
+       <div class="form-group">
+         <h1 class="text-center">Je poste une annonce</h1>
+         <label for="firstname">Prénom</label>
+         <input class="form-control" type="text" name="firstname" placeholder="entrez votre prénom" value="test">
+         <label for="lastname">Nom</label>
+         <input class="form-control" type="text" name="lastname" placeholder="entrez votre nom" value="test">
+         <label for="address">Votre adresse</label>
+         <input class="form-control" type="text" name="address" placeholder="entrez votre adresse"  value="test">
+         <label for="city">Votre ville</label>
+         <input class="form-control" type="text" name="city" placeholder="votre ville" value="test">
+         <label for="zipcode">Code postale</label>
+         <input class="form-control" type="number" name="zipcode" placeholder="entrez votre code postal" value=93500>
+         <label for="email">Email</label>
+         <input class="form-control" type="text" name="email" placeholder="entrez votre email" value="test">
+         <label for="title">Titre de l'annonce</label>
+         <input class="form-control" type="text" name="title" placeholder="entrez le titre de l'annonce" value="test">
+         <label for="description">Description de l'annonce</label>
+         <input class="form-control" type="text" name="description" placeholder="entrez la description de l'annonce" value="test">
+         <label for="photo">Envoyez une Photo</label>
+         <input class="form-control" type="text" name="photo">
+         <div class="form-group">
+           <label for="category">Categorie</label>
+           <select class="form-control" name="category" >
+             <option>véhicule</option>
+             <option selected>informatique</option>
+             <option>bureatique</option>
+             <option>vêtements</option>
+             <option>meubles</option>
+             <option>jouets</option>
+             <option>figurines</option>
+             <option>timbres</option>
+           </select>
+         </div>
+         <div class="form-group">
+           <div>Type d'offre</div>
+           <label for="type">Don</label>
+           <input type="radio" name="type" value="don">
+           <label for="type">Echange</label>
+           <input type="radio" name="type" value="echange" checked>
+         </div>
+         <button type="submit" name="button">Envoyez</button>
+      </div>
+    </form>
+   </body>
+</html>
